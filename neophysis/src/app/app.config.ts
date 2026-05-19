8202b65d36556f0c6fcf1735b333a47f41402a2d
camila <mariacamymendez@gmail.com>
2026-05-18 22:15:32 -0500

    Update project

---
+++ neophysis/src/app/app.config.ts
@@ -0,0 +1,13 @@
+/** @autor LaMendez */
+
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
+};
---
+++ neophysis/src/app/features/register-form/register-form.ts
@@ -0,0 +1,13 @@
+/** @autor LaMendez */
+
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register-form',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './register-form.html',
+  styleUrl: './register-form.scss',
+})
+export class RegisterFormComponent {}
