9d062b2e2df5af0a6a2cfed5034804967c2af8cc
camila <mariacamymendez@gmail.com>
2026-05-20 21:23:13 -0500

    Remove duplicate nested folder

--- neophysis/src/app/features/forum-hero/forum-hero.ts
+++
@@ -1,4 +0,0 @@
-/** @autor LaMendez */
-import { Component } from '@angular/core';
-@Component({ selector: 'app-forum-hero', standalone: true, templateUrl: './forum-hero.html', styleUrl: './forum-hero.scss' })
-export class ForumHeroComponent {}
--- neophysis/src/app/pages/register-page/register-page.ts
+++
@@ -1,7 +0,0 @@
-/** @autor LaMendez */
-import { Component } from '@angular/core';
-import { AuthPanelComponent } from '../../layout/auth-panel/auth-panel';
-import { RegisterFormComponent } from '../../features/register-form/register-form';
-
-@Component({ selector: 'app-register-page', standalone: true, imports: [AuthPanelComponent, RegisterFormComponent], templateUrl: './register-page.html', styleUrl: './register-page.scss' })
-export class RegisterPage {}
